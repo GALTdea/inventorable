57292208e29933514f907e39d469a98f7ceb1a60
Gustavo Valenzuela <GALTdea@gmail.com>
2022-05-11 14:04:27 -0700

    added cities resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "cities#index"
+  resources :cities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
