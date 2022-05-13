b4806eae7e35815a9733c9668bf45945cdcfcf0d
Gustavo Valenzuela <GALTdea@gmail.com>
2022-05-13 14:53:28 -0700

    misc styling

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,21 +1,12 @@
 require 'net/http'
-class City < ApplicationRecord 
+class City < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 
-
-  # def self.weather_api(lat, long )
-  #   url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=2d29671ab8b2ba1d5c84be1d9f943c35"
-  #   uri = URI(url)
-  #   response = Net::HTTP.get_response(uri)
-
-  #   @data = JSON.parse(response.body)
-  # end
 
   def weather_data
     url = "https://api.openweathermap.org/data/2.5/weather?lat=#{self.latitude}&lon=#{self.longitude}&units=imperial&appid=2d29671ab8b2ba1d5c84be1d9f943c35"
     uri = URI(url)
     response = Net::HTTP.get_response(uri)
-     JSON.parse(response.body)
+      JSON.parse(response.body)
   end
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-  # Cities = City.create([
-  #   { name: "Los Angeles", long: "-118.243683", lat:"34.052235", description:""  }, 
-  #   { name: "San Diego", long: "-117.1628", lat:"32.7174", description:""  },
-  #   { name: "Seattle", long: "-122.335167", lat:"47.608013", description:""  },
-  #   { name: "New York", long: "-74.0060", lat:"40.7128", description:""  },
-  #   { name: "Torrance", long: "-118.345413", lat:"33.840763", description:""  }
-  #   ])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Items = Item.create([
+  {name: "Tomatoes", description: "Red and Juicy", city_id: City.last.id, quantity: 15678},
+  {name: "Bananas", description: "Yellow", city_id:  City.find_by(name: "Seattle").id, quantity: 34567},
+  {name: "Tables", description: "Super flat ", city_id: City.find_by(name: "Los Angeles").id, quantity: 234},
+  {name: "Chairs", description: "Restful", city_id: City.find_by(name: "Los Angeles").id, quantity: 567},
+  {name: "Pens", description: "red and blue", city_id:  City.find_by(name: "Torrance").id, quantity: 45678},
+  {name: "Couches", description: "Comfortable", city_id: City.find_by(name: "New York").id, quantity: 234},
+  {name: "Avocados", description: "Green", city_id:  City.find_by(name: "Seattle").id, quantity: 345678},
+  {name: "Lemons", description: "Green and sweet", city_id: City.find_by(name: "New York").id, quantity: 456789},
+  {name: "Desks", description: "Very FLT", city_id:  City.find_by(name: "Torrance").id, quantity: 799},
+  {name: "TV's", description: "Flat tv's", city_id: City.find_by(name: "New York").id, quantity: 234},
+  {name: "Monitors", description: "For your office", city_id:  City.find_by(name: "Torrance").id, quantity: 678},
+  {name: "Pencils", description: "Mechanical", city_id:  City.find_by(name: "Seattle").id, quantity: 100000 }
+])
+
+
 
 # la
 # latitude: 34.052235
